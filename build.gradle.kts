d0c723d041232a7585e7e365176683a968e685a4
diliuskh <diliuskh@gmail.com>
2021-03-30 17:11:32 +0300

    kotlin version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("plugin.jpa") version "1.4.32"
+    kotlin("plugin.jpa") version "1.5.0-M2"
     id("org.springframework.boot") version "2.4.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
+    kotlin("jvm") version "1.5.0-M2"
+    kotlin("plugin.spring") version "1.5.0-M2"
 }
 
 group = "net.dilius"
