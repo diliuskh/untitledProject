f5db3af5e587354ebbf6cce818194ef7b7d29948
diliuskh <diliuskh@gmail.com>
2021-05-10 12:45:44 +0300

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     val kotlinVersion = "1.5.0"
     val springBootVersion = "2.4.5"
@@ -20,10 +18,19 @@ repositories {
     mavenCentral()
 }
 
-val coroutinesVersion = "1.5.0-RC"
+val coroutinesVersion = "1.4.3"
 val junitVersion = "5.8.0-M1"
+
+val mongodbVersion = "3.12.8"
 val kmongoVersion = "4.2.6"
+
 val springDocVersion = "1.5.8"
+
+val opentracingApiVersion = "0.33.0"
+val opentracingSpringVersion = "0.5.9"
+
+val mockitoVersion = "3.9.0"
+val mockitoKotlinVersion = "3.2.0"
 
 dependencies {
     annotationProcessor("org.springframework.boot", "spring-boot-configuration-processor")
@@ -31,6 +38,8 @@ dependencies {
     implementation("org.springdoc","springdoc-openapi-webflux-core", springDocVersion)
     implementation("org.springdoc","springdoc-openapi-webflux-ui", springDocVersion)
     implementation("org.springdoc","springdoc-openapi-kotlin", springDocVersion)
+    api("io.opentracing", "opentracing-api", opentracingApiVersion)
+    implementation("io.opentracing.contrib", "opentracing-spring-cloud-starter", opentracingSpringVersion)
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin")
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
@@ -38,13 +47,19 @@ dependencies {
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutinesVersion)
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-reactor", coroutinesVersion)
 
+    implementation("org.springframework.boot", "spring-boot-starter-data-mongodb-reactive")
     implementation("org.litote.kmongo", "kmongo-coroutine", kmongoVersion)
     implementation("org.litote.kmongo", "kmongo-reactor", kmongoVersion)
+    implementation("org.mongodb", "mongodb-driver-async", mongodbVersion)
 
     runtimeOnly("org.springframework.boot", "spring-boot-devtools")
 
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter", "junit-jupiter")
+    testImplementation("org.junit.jupiter", "junit-jupiter-params")
+    testImplementation("org.mockito", "mockito-core", mockitoVersion)
+    testImplementation("org.mockito", "mockito-junit-jupiter", mockitoVersion)
+    testImplementation("org.mockito.kotlin", "mockito-kotlin", mockitoKotlinVersion)
     testImplementation("org.springframework.boot", "spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlinx", "kotlinx-coroutines-test", coroutinesVersion)
 }
--- src/main/kotlin/net/dilius/untitled/controllers/TestController.kt
+++ src/main/kotlin/net/dilius/untitled/controllers/TestController.kt
@@ -1,13 +1,14 @@
 package net.dilius.untitled.controllers
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.reactor.mono
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
-class TestController {
+class TestController() {
 
     @GetMapping
-    fun test(): String {
-        return "Hello!"
-    }
+    fun test(): Mono<String> = mono { coroutineScope { "Hello!" } }
 }
